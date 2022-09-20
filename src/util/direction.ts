11cb829bd77c95e7d9804b35f33103a3eadc1557
Alex Moon <alex@placechangers.co.uk>
2022-09-20 09:03:21 +0100

    GL-142 - first attempt (broken)

--- src/util/direction.ts
+++ src/util/direction.ts
@@ -15,18 +15,38 @@ function average(values: number[]) {
 }
 
 // very silly implementation of centroid that's good enough for our purposes
-// suggestion: two libraries that do distance, bearing and centroid:
-// https://turfjs.org
-// https://www.npmjs.com/package/geodesy
+// for reference, Worldle uses geolib: https://www.npmjs.com/package/geolib
 function polygonNaiveCentroid(points: number[][]) {
-    const lat = average(points.map(point => point[0]));
-    const lng = average(points.map(point => point[1]));
-    return [lat, lng] as [number, number];
+    const lng = average(points.map(point => point[0]));
+    const lat = average(points.map(point => point[1]));
+    return [lng, lat] as [number, number];
+}
+
+// yoinked from www.movable-type.co.uk/scripts/latlong.html#rhumblines
+function calculateRhumbLineBearing(point1: [number, number], point2: [number, number]) {
+    const lat1 = point1[1];
+    const lat2 = point2[1];
+    const dlat = Math.log(Math.tan(Math.PI/4 + lat2/2) / Math.tan(Math.PI/4 + lat1/2));
+
+    const lng1 = point1[0];
+    const lng2 = point2[0];
+    let dlng = lng2 - lng1; // @todo needs to be <180
+    if (dlng > 180) {
+        dlng = -(360 - dlng);
+    }
+
+    // if dlng over 180° take shorter rhumb line across the anti-meridian:
+    if (Math.abs(dlng) > Math.PI) {
+        dlng = (dlng > 0 ? -1 : 1 ) * (2*Math.PI-dlng);
+    }
+
+    return Math.atan2(dlng, dlat) * 180/Math.PI;
 }
 
 function calcDirection(centroid1: [number, number], centroid2: [number, number]) {
-    const heading = geometry.computeHeading(centroid1, centroid2);
-    if (heading >= 337.5 || heading < 22.5) {
+    const heading = calculateRhumbLineBearing(centroid1, centroid2);
+    console.log('heading', heading);
+    if (heading >= -22.5 && heading < 22.5) {
         return 'north';
     }
     if (heading < 67.5) {
@@ -38,25 +58,28 @@ function calcDirection(centroid1: [number, number], centroid2: [number, number])
     if (heading < 157.5) {
         return 'south east';
     }
-    if (heading < 202.5) {
+    if (heading < -22.5) {
         return 'south';
     }
-    if (heading < 247.5) {
+    if (heading < -67.5) {
         return 'south west';
     }
-    if (heading < 292.5) {
+    if (heading < -112.5) {
         return 'west';
     }
-    if (heading < 337.5) {
+    if (heading < -157.5) {
         return 'north west';
     }
     return 'not sure lol';
 }
 
 export function polygonDirection(country1: Country, country2: Country): string {
+    console.log('guessing direction from', country1.properties.ADMIN, 'to', country2.properties.ADMIN);
     const points1 = polygonPoints(country1);
     const points2 = polygonPoints(country2);
     const centroid1 = polygonNaiveCentroid(points1);
     const centroid2 = polygonNaiveCentroid(points2);
-    return calcDirection(centroid1, centroid2);
+    const result = calcDirection(centroid1, centroid2);
+    console.log('direction', result);
+    return result;
 }
